717d812b9ac11a5003b4f2005ee4667e9406298e
Jason Quaccia <jlquaccia@gmail.com>
2016-12-21 15:35:00 -0800

    image gallery done!

--- app/scripts/services/GalleryImages.js
+++ app/scripts/services/GalleryImages.js
@@ -3,21 +3,21 @@
     var GalleryImages = {};
 
     var images = [
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/FullSizeRender.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/Pole+Backbend.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/eight.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/eleven.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/five.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/four.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/nine.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/one.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/seven.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/six.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/ten.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/thirteen.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/three.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/twelve.png'},
-      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/two.png'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/FullSizeRender.png', id: 0, class: 'img0'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/Pole+Backbend.png', id: 1, class: 'img1'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/eight.png', id: 2, class: 'img2'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/eleven.png', id: 3, class: 'img3'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/five.png', id: 4, class: 'img4'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/four.png', id: 5, class: 'img5'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/nine.png', id: 6, class: 'img6'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/one.png', id: 7, class: 'img7'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/seven.png', id: 8, class: 'img8'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/six.png', id: 9, class: 'img9'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/ten.png', id: 10, class: 'img10'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/thirteen.png', id: 11, class: 'img11'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/three.png', id: 12, class: 'img12'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/twelve.png', id: 13, class: 'img13'},
+      {src: 'https://s3-us-west-1.amazonaws.com/angelas-website/two.png', id: 14, class: 'img14'},
     ];
 
     GalleryImages.getImages = function() {
--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,59 @@
 (function() {
   function HomeCtrl($scope, GalleryImages) {
     $scope.images = GalleryImages.getImages();
+    $scope.imageOverlayStatus = 'closed';
+    $scope.currentEnlargedImage = '';
+    $scope.leftArrow = '';
+    $scope.rightArrow = '';
+    
+    $scope.openImageOverlay = function(index) {
+      $scope.leftArrow = '';
+      $scope.rightArrow = '';
+      $scope.imageOverlayStatus = 'open';
+      $scope.currentEnlargedImage = $scope.images[index];
+
+      if ($scope.currentEnlargedImage.id === 0) {
+        $scope.leftArrow = 'hide';
+      }
+
+      if ($scope.currentEnlargedImage.id === $scope.images.length - 1) {
+        $scope.rightArrow = 'hide';
+      }
+    };
+
+    $scope.closeImageOverlay = function() {
+      $scope.imageOverlayStatus = 'closed';
+    };
+
+    $scope.nextImage = function() {
+      if ($scope.currentEnlargedImage.id < $scope.images.length - 1) {
+        $scope.currentEnlargedImage = $scope.images[$scope.currentEnlargedImage.id + 1];
+
+        if ($scope.currentEnlargedImage.id > 0) {
+          $scope.leftArrow = '';
+        }
+
+        if ($scope.currentEnlargedImage.id === $scope.images.length - 1) {
+          $scope.rightArrow = 'hide';
+        }
+      }
+    };
+
+    $scope.prevImage = function() {
+      if ($scope.currentEnlargedImage.id > 0) {
+        $scope.currentEnlargedImage = $scope.images[$scope.currentEnlargedImage.id - 1];
+
+        if ($scope.currentEnlargedImage.id === 0) {
+          $scope.leftArrow = 'hide';
+        }
+
+        if ($scope.currentEnlargedImage.id < $scope.images.length - 1) {
+          $scope.rightArrow = '';
+        }
+      }
+    };
+
+    console.log($scope.images.length);
   }
 
   angular
